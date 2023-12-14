6fe5cf2ff769b1094c439c2d66f41a85521dd8db
EC2 Topher <89708367+Topher05@users.noreply.github.com>
2023-12-14 19:28:24 +0000

    Gets A full description of a movie review created a method in youtube.py that takes a video id and gives a description from the youtube video. created a fastapi metho that also allows an input of a video id and returns the description. made tests for both methods. issue #3

--- test_main.py
+++ test_main.py
@@ -14,3 +14,11 @@ def test_get_movie_reviews():
 	response = client.get("/moviereviews/cars")
 	assert response.status_code == 200
 	assert "cars" in response.json()[0]["title"].lower() 
+	
+def test_get_movie_review_full_description():
+	response = client.get("/descriptions/rwe9fKfnNS0")
+	assert response.status_code == 200
+	assert response.json() == """CASETiFY's Bounce Cases and Clear Cases are available at casetify.com! Go to http://casetify.com/dylan today to get 15% off your order! Thanks to Casetify for sponsoring this video!
+
+Twitter: https://twitter.com/theDMatthews
+Instagram: https://www.instagram.com/neat_dylan/?hl=en"""
--- test_youtube.py
+++ test_youtube.py
@@ -4,4 +4,12 @@ from youtube import Youtube
 def test_movie_review_search():
 	search_response = Youtube.movie_review_search('Cars')
 	assert "cars" in search_response[0]["title"].lower()
+
+def test_movie_review_full_description():
+	list_response = Youtube.movie_review_full_description('rwe9fKfnNS0')
+	assert list_response == """CASETiFY's Bounce Cases and Clear Cases are available at casetify.com! Go to http://casetify.com/dylan today to get 15% off your order! Thanks to Casetify for sponsoring this video!
+
+Twitter: https://twitter.com/theDMatthews
+Instagram: https://www.instagram.com/neat_dylan/?hl=en"""
+
 	
